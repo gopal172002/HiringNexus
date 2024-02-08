ccb4def52fa54e357fd4405493c6ccf98ad0c85c
gopal172002 <gopalkalsiya1h@gmail.com>
2024-02-08 19:05:00 +0530

    first commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from "../components/Login/Login.jsx"
+// Define a simple React component
+const LoginPage = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+};
+export default LoginPage;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Signup from "../components/Signup/Signup.jsx"
+// Define a simple React component
+const SignupPagePage = () => {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  );
+};
+export default SignupPagePage;
