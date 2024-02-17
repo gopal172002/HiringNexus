1adb778ef17da1b33c4d4f581be2e874d316c433
Deepak Patidar <deepak2025patidar@gmail.com>
2024-02-17 21:07:15 +0530

    fix

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 // import Login from "../components/Login/Login.jsx"
 import Headers from "../components/Layout/Header.jsx"
 import Footer from "../components/Layout/Footer.jsx"
+import Contact from '../components/contact/Contact.jsx';
 // Define a simple React component
 const ContactPage = () => {
   return (
     <div>
     <Headers activeheading={1}/>
-      CONTACT
+     <Contact/>
       <Footer/>
     </div>
   );
